c6194e0fbbcf23eb473e647fb110f59de0f8787e
jesper <jesper.eisfeldt@scilifelab.se>
2021-12-16 14:27:11 +0100

    modified:   README.md 	new file:   esypesy.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # esypesy
 Script for generating esy file from starlims input
+
+
+put your starlims file in the esypesy folder, then click on the esypesy script.
+
+
---
+++ esypesy.py
@@ -0,0 +1,22 @@
+import xlrd
+import os
+
+wd=os.path.dirname(os.path.realpath(__file__))
+
+files = os.listdir(wd)
+
+for f in files:
+	if f.endswith("star.xls"):
+		outfile=f.replace("_star.xls",".esy")
+		o=open(outfile,"w")
+		wb = xlrd.open_workbook(f)
+		sheet = wb.sheet_by_index(0)
+		for i in range(1,sheet.nrows):
+			col="{}0{}".format(sheet.cell_value(i, 13),int(sheet.cell_value(i, 14)))
+			sample=sheet.cell_value(i, 0).strip()
+			if sample.startswith("PCR-"):
+				sample=sample.replace("PCR-","")
+			o.write("{}/r/{}\n".format( col,sample))
+		o.write("/r/")
+		o.close()
+		
